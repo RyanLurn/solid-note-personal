506da8962d823ab64009a01dc0b792f33c0e5c47
Ryan Luong <lumintriet@gmail.com>
2025-09-26 16:31:19 +0700

    Setup TanStack Start.

---
+++ src/routes/__root.tsx
@@ -0,0 +1,49 @@
+/// <reference types="vite/client" />
+
+import {
+  createRootRoute,
+  HeadContent,
+  Outlet,
+  Scripts,
+} from "@tanstack/react-router";
+import type { ReactNode } from "react";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        content: "width=device-width, initial-scale=1",
+        name: "viewport",
+      },
+      {
+        title: "Solid Note",
+      },
+    ],
+  }),
+});
+
+function RootComponent() {
+  return (
+    <RootDocument>
+      <Outlet />
+    </RootDocument>
+  );
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  );
+}
